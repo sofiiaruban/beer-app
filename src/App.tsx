3169b72920bb9b65a277312c05aa1c5c79531782
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-13 20:49:33 +0300

    feat(app): Add fetch func

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import BeerInfo from "./pages/BeerInfo";
+import Beer from "./types/types";
+
+const fetchData = async (url: string): Promise<Array<string> | Array<Beer>> => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 const App: React.FC = () => {
   return (
