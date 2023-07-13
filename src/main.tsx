5851e394e0918d6fd3b7bf65cbb30ad1e94361f3
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-13 20:01:37 +0300

    feat(main): Add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { HashRouter } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <QueryClientProvider client={queryClient}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </QueryClientProvider>
 );
