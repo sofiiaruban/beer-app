18c423d27be4ff19477aee077c37a5843b1b83b7
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-14 18:33:50 +0300

    feat(types): Update store state props

--- src/types/StoreStateProps.ts
+++ src/types/StoreStateProps.ts
@@ -3,4 +3,6 @@ import Beer from "./Beer";
 export default interface StoreStateProps {
   beers: Beer[];
   setBeers: (beers: Beer[]) => void;
+  selectedBeerId: number | null;
+  setSelectedBeerId: (id: number | null) => void;
 }
